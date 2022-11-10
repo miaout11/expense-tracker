5b1e6c18b5dd65592b44835fd863ca5705d80bb1
miaout11 <miaout.11@gmail.com>
2022-11-10 21:28:25 +0800

    add user modules & login/register page

---
+++ routes/modules/users.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/user')
+
+// login page
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// register page
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+module.exports = router
