4725890fe239d2700b2892c8a2fc9ebce010642b
miaout11 <miaout.11@gmail.com>
2022-11-10 21:41:58 +0800

    add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -11,5 +11,29 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
   res.render('register')
 })
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect('/'))
+          .catch(error => console.log(error))
+      }
+    })
+})
 
 module.exports = router
