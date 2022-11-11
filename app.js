1377617bff88faf181b033782763475704896e1e
miaout11 <miaout.11@gmail.com>
2022-11-11 20:08:56 +0800

    add method-override

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const methodOverride = require('method-override')
 
 const routes = require('./routes')
 
@@ -19,6 +20,8 @@ app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 // setting static files
 app.use(express.static('public'))
+// setting methodOverride
+app.use(methodOverride('_method'))
 // setting express-session
 app.use(session({
   secret: 'ThisIsMySecret',
