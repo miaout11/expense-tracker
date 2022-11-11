42abf900ea20915b4c115588654f6c0cbaac4706
miaout11 <miaout.11@gmail.com>
2022-11-11 22:14:44 +0800

    add login warning message

--- config/passport.js
+++ config/passport.js
@@ -9,16 +9,16 @@ module.exports = app => {
   app.use(passport.session())
 
   // setting Localstrategy login
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         // check user exist
         if (!user) {
-          return done(null, false, { message: '這個電子信箱還未註冊過!' })
+          return done(null, false, req.flash('warning_msg', '這個Email還沒有註冊過。'))
         }
         // check account data
         if (user.password !== password) {
-          return done(null, false, { message: '電子信箱或密碼錯誤!' })
+          return done(null, false, req.flash('warning_msg', '密碼或Email錯誤。'))
         }
         return done(null, user)
       })
